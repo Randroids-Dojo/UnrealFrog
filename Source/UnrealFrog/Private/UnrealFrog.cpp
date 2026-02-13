05321b4cf52929b08964555fa051d1635bda9131
Randy Lutcavich <randy@elevationmobile.com>
2026-02-12 22:21:31 -0600

    chore: scaffold UE5 project structure for UnrealFrog
    Set up the foundational project: .uproject targeting 5.4, game/editor
    targets, primary game module with Build.cs (Core, CoreUObject, Engine,
    InputCore, EnhancedInput), Config files with Enhanced Input defaults,
    and Public/Private/Core directory skeleton.

---
+++ Source/UnrealFrog/Private/UnrealFrog.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealFrog.h"
+
+#define LOCTEXT_NAMESPACE "FUnrealFrogModule"
+
+void FUnrealFrogModule::StartupModule()
+{
+}
+
+void FUnrealFrogModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FUnrealFrogModule, UnrealFrog, "UnrealFrog");
---
+++ Source/UnrealFrog/UnrealFrog.Build.cs
@@ -0,0 +1,25 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class UnrealFrog : ModuleRules
+{
+	public UnrealFrog(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.AddRange(new string[] { "Public" });
+		PrivateIncludePaths.AddRange(new string[] { "Private" });
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+	}
+}
